071166415c65e24ae23bc4642066ad1b7f9b04f2
sanderdemeyer <sander.demeyer@ugent.be>
2022-07-08 13:27:57 +0200

    Aanpassingen vrijdag 8/07
    Bij car functies toegevoegd, dqn herschreven

--- car.py
+++ car.py
@@ -19,8 +19,10 @@ class Car:#//cars are circles to make collision easy.
         self.obs_angles = [-m.pi/3, 0, m.pi/3]#the angels we do observations on relative to the direction the car is facing.
         self.obs = [0, 0, 0]
         
+    def __repr__(self):
+        return f"Position: ({self.x}, {self.y}) \nRotation: {self.rot} \nMomentum: {self.momentum}" 
+
     def Move(self):
-        #TO DO
         if self.momentum < 0:
             self.momentum = 0
 
@@ -34,6 +36,27 @@ class Car:#//cars are circles to make collision easy.
         self.x += m.cos(self.rot)*speed
         self.y += m.sin(self.rot)*speed
 
+    def State(self):
+        obs = self.obs
+        return [self.x, self.y, self.rot, self.momentum, self.ang_momentum, obs[0], obs[1], obs[2]]
+
+    def Next_state(self, action):
+        #x, y, rot, momentum, ang_momentum, obs_0, obs_1, obs_2 = state            
+        self.Steer_ai(action)
+        print(self.momentum)
+        print(max_snelheid)
+        speed = self.momentum/np.sqrt(1+(2*self.momentum/max_snelheid)**2) + max_snelheid/2
+        ang_speed = self.ang_momentum/np.sqrt(1+(self.ang_momentum/max_rot_snelheid)**2)
+        self.rot += self.ang_speed
+        self.x += m.cos(self.rot)*speed
+        self.y += m.sin(self.rot)*speed
+        self.Obs()
+
+    def Reward(self):
+        return self.x**2 + self.y**2
+
+    def Done(self):
+        return False
 
     def Obs(self, env:track):#updates observations.
         self.obs = [env.distance((self.x, self.y), self.rot+angle) for angle in self.obs_angles]
--- dqn.py
+++ dqn.py
@@ -1,3 +1,5 @@
+#from fcntl import F_GETFD
+from lib2to3.pgen2.pgen import DFAState
 import random
 import numpy as np
 import torch
@@ -5,56 +7,98 @@ import torch.nn as nn
 import torch.nn.functional as F
 from collections import deque
 
+from car import Car
+
+auto1 = Car()
+repr(auto1)
+
 class DQN(nn.Module):
     def __init__(self):
         super().__init__()
         # De 8 inputs zijn x, y, rot, momentum, ang_momentum, obs[0], obs[1], obs[2]
-        # De 2 outputs zijn kracht, torque
+        # De 4 outputs zijn telkens 0 of 1, afhankelijk of de pijlen 'Boven', 'Rechts', 'Onder', 'Boven'
         self.fc1 = nn.Linear(8, 30)
         self.fc2 = nn.Linear(30, 50)
-        self.fc3 = nn.Linear(50, 2)
-def forward(self, x):
-    x = self.fc1(x)
-    x = F.relu(x)
-    x = self.fc2(x)
-    x = F.relu(x)
-    x = self.fc3(x)
-    return x
+        self.fc3 = nn.Linear(50, 4)
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        return x
+
+"""
+print('hey')
+dqn = DQN()
+tens = torch.tensor([[1, 2, 3, 4, 5, 6, 7, 8]], dtype=torch.float32)
+tens2 = torch.tensor([[1, 2, 3, 4, 5, 6, 7, 8]], dtype=torch.float32)
+tens3 = torch.tensor([[1, 2, 3, 4, 5, 6, 7, 10]], dtype=torch.float32)
+#torch.tensor(np.reshape(tens, [1, 8]), dtype = torch.float32)
+
+print(tens)
+y = dqn(tens)
+y2 = dqn(tens2)
+y3 = dqn(tens3)
+
+print('hey')
+for i in range(4):
+    print(y[0][i])
+print('he')
+for i in range(4):
+    print(y2[0][i])
+print('hoi')
+for i in range(4):
+    print(y3[0][i])
+"""
 
 class DQN_solver:
-    def __init__(self, gamma = 1, n_episodes = 100, eps_start = 1, eps_end = 0.1, eps_decay = 0):
+    def __init__(self, gamma = 1, n_episodes = 100, eps_start = 1, eps_end = 0.1, eps_decay = 0.995, alpha = 0.01, alpha_end = 0, alpha_decay = 0.01, batch_size = 64, quiet = False):
         self.gamma = gamma
         self.n_episodes = n_episodes
         self.eps_start = eps_start
         self.eps_end = eps_end
         self.eps_decay = eps_decay
         self.memory = deque(maxlen=100000)
+        self.alpha = alpha
+        self.alpha_end = alpha_end
+        self.alpha_decay = alpha_decay
         #Initialiseer model
         self.dqn = DQN()
         self.criterion = torch.nn.MSELoss()
         self.opt = torch.optim.Adam(self.dqn.parameters(), lr=0.01)
+        self.batch_size = batch_size
+        self.quiet = quiet
     def get_epsilon(self, t):
-        return eps_end + (eps_start-eps_end)*np.exp(-t/eps_decay)
+        return self.eps_end + (self.eps_start-self.eps_end)*np.exp(-t/self.eps_decay)
+
+    def get_alpha(self, t):
+        return self.alpha_end + (self.eps_alpha-self.eps_alpha)*np.exp(-t/self.eps_alpha)    
 
     def preprocess_state(self, state):
         return torch.tensor(np.reshape(state, [1, 8]), dtype = torch.float32)
     
     def choose_action(self, state, epsilon):
+        epsilon = 1
         if np.random.random() < epsilon:
-            pass #random
+            print('random')
+            action = (random.choice([True, False]), random.choice([True, False]))
+            print(action)
+            return action
         else:
+            print('niet random')
             with torch.no_grad():
                 return torch.argmax(self.dqn(state)).numpy()
     
-    def remember(self, state, actie, reward, next_state, done):
+    def remember(self, state, action, reward, next_state, done):
         reward = torch.tensor(reward)
-        self.memory.append((state, actie, reward, next_state, done))
+        self.memory.append((state, action, reward, next_state, done))
     
     def replay(self, batch_size):
         #Nog niet veranderd
         y_batch, y_target_batch = [], []
         minibatch = random.sample(self.memory, min(len(self.memory), batch_size))
-        for state, actie, reward, next_state, done in minibatch:
+        for state, action, reward, next_state, done in minibatch:
             y = self.dqn(state)
             y_target = y.clone().detach()
             with torch.no_grad():
@@ -70,9 +114,33 @@ class DQN_solver:
         loss.backward()
         self.opt.step()        
         
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
+        # if self.epsilon > self.epsilon_min:
+        #     self.epsilon *= self.epsilon_decay
 
+    def run(self):
+        scores = deque(maxlen=100)
+    
+        for e in range(self.n_episodes):
+            print(auto1)
+            state = self.preprocess_state(auto1.State())
+            print(f"state is {state}")
+            done = False
+            i = 0
+            while not done:
+                if e%100 == 0 and not self.quiet:
+                    pass #render
+                action = self.choose_action(state, self.get_epsilon(e))
+                print(f"action is {action}")
+                #next_state, reward, done, _ = _ #self.env.step(action)
+                next_state, reward, done = auto1.Next_state(action), auto1.Reward(), auto1.Done()
+                next_state = self.preprocess_state(next_state)
+                self.remember(state, action, reward, next_state, done)
+                state = next_state
+                i += 1
+            scores.append(i)
+        mean_score = np.mean(scores)
 
+agent = DQN_solver()
+agent.run()
 
 
